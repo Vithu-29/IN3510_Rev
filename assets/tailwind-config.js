a9d330c27cc14f9edab449e8078eb101f7b6d3b4
Vithushan Kanesamoorthy <vithuvithush44@gmail.com>
2025-11-30 13:49:49 +0530

    Feat:lecture 1 short note add

---
+++ assets/tailwind-config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    50: '#f0fdfa',
+                    100: '#ccfbf1',
+                    600: '#0d9488',
+                    800: '#115e59',
+                    900: '#134e4a',
+                },
+                teal: {
+                    600: '#0d9488',
+                    700: '#0f766e',
+                },
+                slate: {
+                    800: '#1e293b',
+                }
+            },
+            fontFamily: {
+                sans: ['Inter', 'system-ui', 'sans-serif'],
+                mono: ['Fira Code', 'monospace'],
+            }
+        }
+    }
+};
